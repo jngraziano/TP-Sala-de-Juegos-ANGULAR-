1ebedad6758ca9118ea095aca1292f5352a5060c
Julian Graziano <jngraziano@gmail.com>
2019-09-19 18:15:03 -0300

    AdivinaNFIX-PptNOK

--- src/app/componentes/adivina-el-numero/adivina-el-numero.component.ts
+++ src/app/componentes/adivina-el-numero/adivina-el-numero.component.ts
@@ -17,7 +17,8 @@ export class AdivinaElNumeroComponent implements OnInit {
  
   constructor() { 
     this.nuevoJuego = new JuegoAdivina();
-    console.info("numero Secreto:",this.nuevoJuego.numeroSecreto);  
+    this.contador = 0;
+    console.info("numero Secreto:",this.nuevoJuego.numeroSecreto+" - Contador; "+this.contador);  
     this.ocultarVerificar=false;
   }
   generarnumero() {
@@ -27,8 +28,9 @@ export class AdivinaElNumeroComponent implements OnInit {
   verificar()
   {
     this.contador++;
+    console.info("El contador va: :",this.contador);  
     this.ocultarVerificar=true;
-    console.info("numero Secreto:",this.nuevoJuego.gano);  
+
     if (this.nuevoJuego.verificar()){
       
       this.enviarJuego.emit(this.nuevoJuego);
@@ -40,13 +42,13 @@ export class AdivinaElNumeroComponent implements OnInit {
       let mensaje:string;
       switch (this.contador) {
         case 1:
-          mensaje="No, intento fallido, animo";
+          mensaje="1er intento fallido, animo.";
           break;
           case 2:
-          mensaje="No,Te estaras Acercando???";
+          mensaje="¿Te estaras Acercando?";
           break;
           case 3:
-          mensaje="No es, Yo crei que la tercera era la vencida.";
+          mensaje="No es. Crei que la tercera era la vencida.";
           break;
           case 4:
           mensaje="No era el  "+this.nuevoJuego.numeroIngresado;
@@ -55,18 +57,27 @@ export class AdivinaElNumeroComponent implements OnInit {
           mensaje=" intentos y nada.";
           break;
           case 6:
-          mensaje="Afortunado en el amor";
+          mensaje="Ultima posibilidad.";
           break;
       
         default:
-            mensaje="Ya le erraste "+ this.contador+" veces";
+            mensaje="Ya le erraste "+ this.contador+" veces.";
           break;
       }
       this.MostarMensaje("#"+this.contador+" "+mensaje+" ayuda :"+this.nuevoJuego.retornarAyuda());
      
 
     }
-    console.info("numero Secreto:",this.nuevoJuego.gano);  
+
+    if(this.contador == 7) {
+      this.enviarJuego.emit(this.nuevoJuego);
+      this.MostarMensaje("PERDISTE",false);
+      this.nuevoJuego.numeroSecreto=0;
+
+    }
+    // console.info("numero Secreto:",this.nuevoJuego.gano);  
+    console.info("¿Gano?",this.nuevoJuego.gano);  
+    return this.nuevoJuego.gano;
   }  
 
   MostarMensaje(mensaje:string="este es el mensaje",ganador:boolean=false) {
@@ -84,8 +95,12 @@ export class AdivinaElNumeroComponent implements OnInit {
       modelo.ocultarVerificar=false;
      }, 3000);
     console.info("objeto",x);
-  
    }  
+
+   GanoJuego()
+   {
+    console.info("ESTOY EN GANO ADIVINA EL NUMERO CON EVENT");
+   }
   ngOnInit() {
   }
 
