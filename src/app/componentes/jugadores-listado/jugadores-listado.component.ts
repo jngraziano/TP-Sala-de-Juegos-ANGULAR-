e83cfd99d33a998026d3cfd94bc7b5ae10dc4524
Julian Graziano <jngraziano@gmail.com>
2019-10-10 12:23:24 -0300

    UpdateUsuariosLOK

--- src/app/componentes/jugadores-listado/jugadores-listado.component.ts
+++ src/app/componentes/jugadores-listado/jugadores-listado.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,ElementRef } from '@angular/core';
 import { JugadoresService } from '../../servicios/jugadores.service';
+
+import { FirebaseService } from '../../servicios/firebase.service';
 @Component({
   selector: 'app-jugadores-listado',
   templateUrl: './jugadores-listado.component.html',
@@ -7,11 +9,15 @@ import { JugadoresService } from '../../servicios/jugadores.service';
 })
 export class JugadoresListadoComponent implements OnInit {
 
-  listado:any
+  listado:any;
+  // usuarios:any=[];
   miJugadoresServicio:JugadoresService
   
-    constructor(serviceJugadores:JugadoresService) {
+    constructor(serviceJugadores:JugadoresService,private baseService:FirebaseService,
+      private elementRef: ElementRef) {
+      this.traigoUsuarios();
       this.miJugadoresServicio = serviceJugadores;
+
       
     }
     
@@ -19,6 +25,9 @@ export class JugadoresListadoComponent implements OnInit {
 
   ngOnInit() {
   }
+  ngAfterViewInit(){
+    this.elementRef.nativeElement.ownerDocument.body.style.backgroundColor = 'darkgrey';
+ }
 
 
   TraerTodos(){
@@ -29,19 +38,37 @@ export class JugadoresListadoComponent implements OnInit {
 
     })
   }
-  TraerGanadores(){
-    this.miJugadoresServicio.traertodos('jugadores/','ganadores').then(data=>{
-      //console.info("jugadores listado",(data));
-      this.listado= data;
 
-    })
-  }
-  TraerPerdedores(){
-    this.miJugadoresServicio.traertodos('jugadores/','perdedores').then(data=>{
-      //console.info("jugadores listado",(data));
-      this.listado= data;
+  traigoUsuarios(){
+    this.baseService.getItems("salaJuegos/Usuarios").then(users => {
+      // setTimeout(() => this.spinner = false, 2000);
+     
+      // console.log(this.usuarios);
+      // console.log(users);
 
-    })
+      this.listado = users;
+
+      // let usuarioLogueado = this.usuarios.find(elem => (elem.email == this.cuenta.email && elem.clave == this.cuenta.clave));
+      // console.log(usuarioLogueado);
+      // console.log(usuarioLogueado);
+      // console.log(this.cuenta);
+      
+    });
   }
+  }
+  // TraerGanadores(){
+  //   this.miJugadoresServicio.traertodos('jugadores/','ganadores').then(data=>{
+  //     //console.info("jugadores listado",(data));
+  //     this.listado= data;
+
+  //   })
+  // }
+  // TraerPerdedores(){
+  //   this.miJugadoresServicio.traertodos('jugadores/','perdedores').then(data=>{
+  //     //console.info("jugadores listado",(data));
+  //     this.listado= data;
+
+  //   })
+  // }
 
 }
