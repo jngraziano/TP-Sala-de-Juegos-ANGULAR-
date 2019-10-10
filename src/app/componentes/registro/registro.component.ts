66e432c3526038230d994baa9f9f28ee929682c7
Julian Graziano <jngraziano@gmail.com>
2019-10-10 05:19:20 -0300

    RegistroOK

--- src/app/componentes/registro/registro.component.ts
+++ src/app/componentes/registro/registro.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+import { FirebaseService } from '../../servicios/firebase.service';
 // import {  } from "..";
 //para poder hacer las validaciones
 //import { Validators, FormBuilder, FormControl, FormGroup} from '@angular/forms';
@@ -15,18 +16,66 @@ export class RegistroComponent implements OnInit {
   formRegistro:FormGroup=this.miConstructor.group({
     usuario:this.email
   });*/
+  listadoUsuarios:any=[];
 
   public usuario  =
   {
     "email" : "",
-    "psw" : "",
-    "pswR" : ""
+    "password" : "",
+    "passwordR" : ""
 
 
   }
-  constructor( ) { }
+   error: boolean = false;
+   errorP: boolean = false;
+   success: boolean = false;
+  constructor(private baseService:FirebaseService ) {
+
+   }
 
   ngOnInit() {
   }
+
+  registrarme(){
+
+    if(this.usuario.password != this.usuario.passwordR)
+    {
+      this.errorP = true;
+    }
+    else{
+      this.errorP = false;
+    }
+  
+    this.baseService.getItems("salaJuegos/Usuarios").then(users => {
+     
+      this.listadoUsuarios = users;
+
+      let usuarioLogueado = this.listadoUsuarios.find(elem => (elem.email == this.usuario.email));
+      console.log(usuarioLogueado);
+   
+      if (usuarioLogueado !== undefined) {
+        this.error = true;
+      
+        
+      }
+      else{
+        this.error = false;
+        let usuarioCargar = {
+          "email":this.usuario.email,
+          "clave":this.usuario.password
+        }
+        this.baseService.addItem('salaJuegos/Usuarios', usuarioCargar); 
+        this.usuario.email= "";
+        this.usuario.password= "";
+        this.usuario.passwordR= "";
+
+        this.success = true;
+      }
+  
+  
+  });
+  
+  }
+  
 
 }
