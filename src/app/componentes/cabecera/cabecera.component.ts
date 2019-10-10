e16fafaaba0b48e7dfe0e17823180ee64d73d22e
Julian Graziano <jngraziano@gmail.com>
2019-10-10 09:56:53 -0300

    CabeceraCambiosXusuario

--- src/app/componentes/cabecera/cabecera.component.ts
+++ src/app/componentes/cabecera/cabecera.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
+import undefined = require('firebase/empty-import');
 
 @Component({
   selector: 'app-cabecera',
@@ -8,10 +9,23 @@ import { Router, ActivatedRoute } from '@angular/router';
 })
 export class CabeceraComponent implements OnInit {
     navbarOpen = false;
-  
+    usuarioLogueado:any;
+    usuarioLOG:boolean = false;
+    usuarioNologeado:boolean = true;
+
 
   constructor(router: Router) {
     this.navbarOpen = false;
+    this.usuarioLogueado = JSON.parse(sessionStorage.getItem('Usuarios'));
+    if(this.usuarioLogueado != undefined)
+    {
+      this.usuarioNologeado = false;
+      this.usuarioLOG = true;
+    }
+    else{
+      this.usuarioNologeado = true;
+      this.usuarioLOG = false;
+    }
 
    }
 
